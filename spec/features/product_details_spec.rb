919c6552fbf301145b4eca220b2e31e9def20a7a
Maggie Holbling <maggieholbl@gmail.com>
2019-10-23 00:47:09 +0000

    add product details test

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to product detailspage", type: :feature, js: true do
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see all a product details page" do
+    # ACT
+    visit root_path
+    first(:link, 'Details').click
+    
+
+    # VERIFY
+    expect(page).to have_css 'article.product-detail', count: 1
+    save_screenshot('image2.png')
+  end
+end
